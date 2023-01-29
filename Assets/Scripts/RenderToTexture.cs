ae64f7edb78dc77f312dbba7248379b5344afe65
Himanchal Sharma <himanchal.sharma@gridraster.com>
2023-01-29 22:47:31 +0530

    Latest

--- Assets/Scripts/RenderToTexture.cs
+++ Assets/Scripts/RenderToTexture.cs
@@ -32,67 +32,60 @@ public class RenderToTexture : MonoBehaviour
 
     void Start()
     {
-
-
-
-
-
-
-
-
         cam = GetComponent<Camera>();
-        //cam.SetStereoViewMatrix(Camera.StereoscopicEye.Left, Matrix4x4.identity);
-        //cam.SetStereoViewMatrix(Camera.StereoscopicEye.Right, cam.GetStereoViewMatrix(Camera.StereoscopicEye.Right));
-        Debug.Log(XRSettings.enabled);
+        
         StartSubsystem();
-        Debug.Log(XRSettings.enabled);
-        colorTex = new RenderTexture(Screen.width, Screen.height, 16, RenderTextureFormat.ARGB32);
+        colorTex = new RenderTexture(1067, 269, 32);
         colorTex.autoGenerateMips = false;
         colorTex.anisoLevel = 0;
         colorTex.name = "ColorTexture";
-        textinfo.text = "" + XRSettings.enabled;
-        //if (XRSettings.enabled)
-        //{
-        //    colorTex.vrUsage = VRTextureUsage.TwoEyes;
-        //  //  colorTex.width *= 2;
-        SubsystemManager.GetSubsystems(displays);
-       // textinfo.text = "" + displays[0].supportedTextureLayouts;
-        displays[0].singlePassRenderingDisabled = true;
-       //colorTex.vrUsage = VRTextureUsage.TwoEyes;
-        //    if (displays.Count > 0)
-        //    {
-        //       // displays[0].textureLayout = TextureLayout.Texture2DArray;
-        //           Debug.Log(displays[0].singlePassRenderingDisabled);
-        //       // textinfo.text = ""+ displays[0].textureLayout;
-        //    }
+        colorTex.useDynamicScale = true;
+/*        colorTex.dimension = TextureDimension.Tex2DArray;
+        colorTex.enableRandomWrite = true;
+        colorTex.volumeDepth = 2;*/ 
+        colorTex.vrUsage = VRTextureUsage.TwoEyes;
+        depthTex = new RenderTexture(1067,269,32);
+        depthTex.autoGenerateMips = false;
+        depthTex.anisoLevel = 0;
+        depthTex.name = "DepthTexture";
+        depthTex.useDynamicScale = true;
+/*        depthTex.dimension = TextureDimension.Tex2DArray;
+        depthTex.enableRandomWrite = true;
+        depthTex.volumeDepth = 2;*/
+      //  depthTex.vrUsage = VRTextureUsage.TwoEyes;
+      //  depthTex.width *= 2;
 
-        //}
+        SubsystemManager.GetSubsystems(displays);
+        Debug.Log(UnityEngine.Application.persistentDataPath);
+        displays[0].textureLayout = TextureLayout.SingleTexture2D;
+/*        Debug.Log(Screen.height);
+        Debug.Log(Screen.width);*/
 
         colorTex.Create();
-        //colorTex1.Create();
+        depthTex.Create();
 
-        //  cam.depthTextureMode |= DepthTextureMode.Depth;
+       //  cam.depthTextureMode |= DepthTextureMode.Depth;
         //cam.targetTexture = colorTex;
         //cam.SetTargetBuffers(colorTex.colorBuffer, depthTex.depthBuffer);
 
-        //PostprocessShader = Shader.Find("GR/ExtractDepth");
+        PostprocessShader = Shader.Find("GR/ExtractDepth");
 
-        //if (PostprocessShader == null)
-        //    PostprocessShader = Resources.Load<Shader>("ExtractDepth.shader");
+        if (PostprocessShader == null)
+            PostprocessShader = Resources.Load<Shader>("ExtractDepth.shader");
 
-        //if (PostprocessShader)
-        //{
-        //    PostprocessMaterial = new Material(PostprocessShader);
-        //    PostprocessMaterial.enableInstancing = true;
-        //    Debug.Log("[GR]: Found Shader for Depth Post Processing!");
-        //}
-        //else
-        //{
-        //    Debug.LogError("[GR]: Did not find Shader for Depth Post Processing!");
-        //}
+        if (PostprocessShader)
+        {
+            PostprocessMaterial = new Material(PostprocessShader);
+            PostprocessMaterial.enableInstancing = false;
+            Debug.Log("[GR]: Found Shader for Depth Post Processing!");
+        }
+        else
+        {
+            Debug.LogError("[GR]: Did not find Shader for Depth Post Processing!");
+        }
 
 
-        
+
         StartCoroutine(createtexture());
     }
 
@@ -114,11 +107,24 @@ public class RenderToTexture : MonoBehaviour
             once = false;
         }
         elapsed += Time.deltaTime;
-        colorTex = displays[0].GetRenderTextureForRenderPass(0);
+      //  Graphics.CopyTexture(displays[0].GetRenderTextureForRenderPass(0),0,0,colorTex,0,0);
+         colorTex = displays[0].GetRenderTextureForRenderPass(0);
+      //  depthTex = displays[0].GetRenderTextureForRenderPass(0);
+        //depthTex = displays[0].GetRenderTextureForRenderPass(1);
+        // Graphics.Blit(displays[0].GetRenderTextureForRenderPass(0), colorTex, PostprocessMaterial);
+         Graphics.Blit(colorTex, depthTex, PostprocessMaterial);
         if (elapsed > 1)
         {
             elapsed = elapsed % 1f;
-
+            //     Debug.Log(displays[0].GetRenderPassCount());
+            /*            Debug.Log(displays[0].GetRenderTextureForRenderPass(0).width);
+                        Debug.Log(displays[0].GetRenderTextureForRenderPass(0).height);
+                        Debug.Log(displays[0].GetRenderTextureForRenderPass(0).volumeDepth);
+                        Debug.Log(colorTex.width);
+                        Debug.Log(colorTex.height);
+                        Debug.Log(colorTex.volumeDepth);*/
+            //  Debug.Log(displays[0].GetRenderTextureForRenderPass(0).useDynamicScale);
+            //     Debug.Log(displays[0].GetRenderTextureForRenderPass(1).width);
             //Debug.Log(cam.GetStereoViewMatrix(Camera.StereoscopicEye.Left));
             //Debug.Log(cam.GetStereoViewMatrix(Camera.StereoscopicEye.Right));
 
@@ -139,31 +145,42 @@ public class RenderToTexture : MonoBehaviour
             //   Debug.Log(cam.GetStereoViewMatrix(Camera.StereoscopicEye.Right));
             //  cam.SetStereoViewMatrix(Camera.StereoscopicEye.Left,Matrix4x4.identity);
             //  Debug.Log(cam.GetStereoViewMatrix(Camera.StereoscopicEye.Left));
-                        XRRenderPass xrp1,xrp2;
-                        displays[0].GetRenderPass(0, out xrp1);
-                        displays[0].GetRenderPass(1, out xrp2);
-            //textinfo.text = "" + xrp.GetRenderParameterCount();
-                        XRRenderParameter xrpr1, xrpr2;
-                        xrp1.GetRenderParameter(cam, 0, out xrpr1);
-                        xrp2.GetRenderParameter(cam, 0, out xrpr2);
-            Debug.Log(xrpr1.projection);
-            Debug.Log(xrpr2.projection);
+            /*                        XRRenderPass xrp1,xrp2;
+                                    displays[0].GetRenderPass(0, out xrp1);
+                                    displays[0].GetRenderPass(1, out xrp2);
+                        //textinfo.text = "" + xrp.GetRenderParameterCount();
+                                    XRRenderParameter xrpr1, xrpr2;
+                                    xrp1.GetRenderParameter(cam, 0, out xrpr1);
+                                    xrp2.GetRenderParameter(cam, 0, out xrpr2);
+                        Debug.Log(xrpr1.projection);
+                        Debug.Log(xrpr2.projection);*/
             //xrp.GetRenderParameter(cam, 1, out xrpr2);
             //Debug.Log(xrpr1.view);
             // Debug.Log(cam.GetStereoProjectionMatrix(Camera.StereoscopicEye.Left)); They are the same
             //Debug.Log(xrpr2 .view);
             // Debug.Log(cam.GetStereoProjectionMatrix(Camera.StereoscopicEye.Right));
-
-            RenderTexture lastActive = RenderTexture.active;
-            RenderTexture.active = displays[0].GetRenderTextureForRenderPass(0);
+           // Debug.Log("HERE1");
+/*            RenderTexture lastActive = RenderTexture.active;
+            RenderTexture.active = colorTex;
             Texture2D texy = new Texture2D(RenderTexture.active.width, RenderTexture.active.height);
             texy.ReadPixels(new Rect(0, 0, RenderTexture.active.width, RenderTexture.active.height), 0, 0);
             texy.Apply();
             byte[] dat = texy.EncodeToPNG();
-            File.WriteAllBytes(UnityEngine.Application.persistentDataPath + "/sourceTexture0_" + count + ".png", dat);
+            File.WriteAllBytes(UnityEngine.Application.persistentDataPath + "/colortex0" + count + ".png", dat);
             Destroy(texy);
-            RenderTexture.active = lastActive;
-             Debug.Log("WITTEN");
+          //  Debug.Log("HERE2");
+            RenderTexture.active = depthTex;
+            texy = new Texture2D(RenderTexture.active.width, RenderTexture.active.height);
+            texy.ReadPixels(new Rect(0, 0, RenderTexture.active.width, RenderTexture.active.height), 0, 0);
+            texy.Apply();
+            dat = texy.EncodeToPNG();
+           // Debug.Log("HERE3");
+            File.WriteAllBytes(UnityEngine.Application.persistentDataPath + "/depth0" + count + ".png", dat);
+            Debug.Log("HERE4");
+            Destroy(texy);
+            RenderTexture.active = lastActive;*/
+          //  Debug.Log("WITTEN");
+           // count += 1;
         }
         StartCoroutine(createtexture());
     }
@@ -195,7 +212,7 @@ public class RenderToTexture : MonoBehaviour
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         //if (cam.name != Camera.current.name) return;
-        // Graphics.Blit(source, depthTex, PostprocessMaterial);
+       // Graphics.Blit(source, depthTex, PostprocessMaterial);
         Graphics.Blit(source, destination);
     }
     void setMatrix()
---
+++ Assets/Shaders/ExtractDepth.shader
@@ -0,0 +1,59 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "GR/ExtractDepth" {
+
+	Properties{
+		_gray("Gray Scale", Float) = 0
+	}
+
+		SubShader{
+			Cull Off ZWrite Off ZTest Always
+			Tags { "RenderType" = "Opaque" }
+
+			Pass{
+				CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+				#include "UnityCG.cginc"
+
+
+				float _gray;
+				UNITY_DECLARE_DEPTH_TEXTURE(_CameraDepthTexture);
+				//sampler2D _CameraDepthTexture;
+				//sampler2D _MainTex;
+				half4 _CameraDepthTexture_ST;
+				struct v2f {
+				   float4 pos : SV_POSITION;
+				   float4 scrPos:TEXCOORD1;
+				   float2 uv:TEXCOORD0;
+				   float2 color:COLOR;
+				   UNITY_VERTEX_INPUT_INSTANCE_ID
+				   UNITY_VERTEX_OUTPUT_STEREO
+				};
+
+				v2f vert(appdata_full v) {
+				   v2f o;
+				   UNITY_SETUP_INSTANCE_ID(v);
+				   UNITY_INITIALIZE_OUTPUT(v2f, o);
+				   //UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(v);
+				   TRANSFER_SHADOW_CASTER_NORMALOFFSET(o);
+				   o.pos = UnityObjectToClipPos(v.vertex);
+				//   o.uv = v.uv;
+				   o.scrPos = ComputeScreenPos(o.pos); //UnityStereoScreenSpaceUVAdjust(ComputeScreenPos(o.pos), _CameraDepthTexture_ST); 
+				   return o;
+				}
+				UNITY_DECLARE_SCREENSPACE_TEXTURE(_MainTex);
+				half4 frag(v2f i) : COLOR{
+					UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(i);
+					//#if UNITY_SINGLE_PASS_STEREO
+					//	i.uvgrab.xy = TransformStereoScreenSpaceTex(i.uvgrab.xy, i.uvgrab.w);
+					//#endif
+					// the value is between [0, 1] = [near plane, far plane]
+					float d = Linear01Depth(UNITY_SAMPLE_SCREENSPACE_TEXTURE(_CameraDepthTexture, i.scrPos)); ;//Linear01Depth(tex2Dproj(_CameraDepthTexture, i.scrPos).r);
+					return half4(d, d, d, 1);
+				}
+				ENDCG
+			}
+	}
+		FallBack "Diffuse"
+}
